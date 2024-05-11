f1c57cb15a153bf6dcbc4d461a9a62cf5c19b6fb
Omanshu <114089324+Omanshu209@users.noreply.github.com>
2024-05-11 17:11:51 +0530

    Updated `Classifiers`

--- Deep_Learning/DisneyCharacterClassifier.py
+++ Deep_Learning/DisneyCharacterClassifier.py
@@ -1,7 +1,11 @@
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-from torchvision import transforms, models
+from torch.nn import Linear as nn_Linear
+from torch import max as torch_max
+from torch import load as torch_load
+from torch import no_grad as torch_no_grad
+from torch.cuda import is_available as cuda_is_available
+from torch.nn.functional import softmax as torch_softmax
+from torchvision import transforms
+from torchvision.models import resnet18
 
 labels : dict = {
 	"Buzz Lightyear": 0, 
@@ -32,20 +36,20 @@ class DisneyCharacterClassifier:
 			]
 		)
 		
-		self.device = "cuda" if torch.cuda.is_available() else "cpu"
+		self.device = "cuda" if cuda_is_available() else "cpu"
 		self.Model = self.load_model(model_path)
 		self.Model.eval()
 	
 	def load_model(self, model_path):
-		Model = models.resnet18(pretrained = True)
+		Model = resnet18(pretrained = True)
 		
 		for p in Model.parameters():
 			p.requires_grad = False
 		
-		Model.fc = nn.Linear(512, 10)
+		Model.fc = nn_Linear(512, 10)
 		
 		Model.load_state_dict(
-			torch.load(
+			torch_load(
 				model_path, 
 				map_location = self.device
 			)
@@ -58,13 +62,13 @@ class DisneyCharacterClassifier:
 		return transformed_image
 	
 	def predict(self, transformed_image):
-		with torch.no_grad():
+		with torch_no_grad():
 			y_pred = self.Model(transformed_image.unsqueeze(0).to(self.device))
 			
-			probabilities = F.softmax(y_pred, dim = 1).squeeze().detach().numpy()
+			probabilities = torch_softmax(y_pred, dim = 1).squeeze().detach().numpy()
 			percent_probabilities = {class_label: round(prob.item() * 100) for class_label, prob in zip(labels.keys(), probabilities)}
 			
-			y_pred_tensor = torch.max(y_pred, 1)[1]
+			y_pred_tensor = torch_max(y_pred, 1)[1]
 			y_pred_string = [i for i in labels if labels[i] == y_pred_tensor][0]
 		
 		return (y_pred_string, percent_probabilities)
--- Deep_Learning/DisneyMovieClassifier.py
+++ Deep_Learning/DisneyMovieClassifier.py
@@ -1,7 +1,11 @@
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-from torchvision import transforms, models
+from torch.nn import Linear as nn_Linear
+from torch import argmax as torch_argmax
+from torch import load as torch_load
+from torch import no_grad as torch_no_grad
+from torch.cuda import is_available as cuda_is_available
+from torch.nn.functional import softmax as torch_softmax
+from torchvision import transforms
+from torchvision.models import resnet18
 
 labels : dict = {
 	0: "Coco",
@@ -34,20 +38,20 @@ class DisneyMovieClassifier:
 			]
 		)
 		
-		self.device = "cuda" if torch.cuda.is_available() else "cpu"
+		self.device = "cuda" if cuda_is_available() else "cpu"
 		self.Model = self.load_model(model_path)
 		self.Model.eval()
 	
 	def load_model(self, model_path):
-		Model = models.resnet18(pretrained = True)
+		Model = resnet18(pretrained = True)
 		
 		for p in Model.parameters():
 			p.requires_grad = False
 		
-		Model.fc = nn.Linear(512, 11)
+		Model.fc = nn_Linear(512, 11)
 		
 		Model.load_state_dict(
-			torch.load(
+			torch_load(
 				model_path, 
 				map_location = self.device
 			)
@@ -60,13 +64,13 @@ class DisneyMovieClassifier:
 		return transformed_image
 	
 	def predict(self, transformed_image):
-		with torch.no_grad():
+		with torch_no_grad():
 			y_pred = self.Model(transformed_image.unsqueeze(0).to(self.device))
 			
-			probabilities = F.softmax(y_pred, dim = 1).squeeze().detach().numpy()
+			probabilities = torch_softmax(y_pred, dim = 1).squeeze().detach().numpy()
 			percent_probabilities = {class_label: round(prob.item() * 100) for class_label, prob in zip(labels.values(), probabilities)}
 			
-			y_pred_tensor = int(torch.argmax(y_pred, 1))
+			y_pred_tensor = int(torch_argmax(y_pred, 1))
 			y_pred_string = labels[y_pred_tensor]
 		
 		return (y_pred_string, percent_probabilities)
